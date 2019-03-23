2799ee3fd33424472c85d713f0f2567208ec0dd9
David Liman <limanoit@gmail.com>
2019-03-23 14:06:04 -0700

    change format

--- src/dvliman/handler.clj
+++ src/dvliman/handler.clj
@@ -30,46 +30,45 @@
 
 (defn webflux-article []
   [:section
-   [:h2 "Building a reactive web service with Spring Webflux, Kotlin, and PostgreSQL"]
+   [:h1 "Building a reactive web service with Spring Webflux, Kotlin, and PostgreSQL"]
    [:p "This post shows how to create a reactive web service with Spring Webflux, Kotlin, PostgreSQL"]
-   [:blockquote [:p "For the context, " [:a {:href "https://spring.io/"} "Spring Framework 5"] " introduced the so-called "
-                 [:strong "Reactive Stack"] ". The " [:code "reactive"] " keyword here refers to the "
-                 [:a {:href "https://www.reactivemanifesto.org/"} "Reactive Manifesto"]
-                 ", which is a standard " [:a {:href "https://github.com/reactive-streams/reactive-streams-jvm/blob/v1.0.0/README.md"}
-                                           "specification"] " for asynchronous stream processing with non-blocking back-pressure in JVM languages (and javascript).
-    In short, Spring Webflux is a non-blocking web framework that uses " [:a {:href "https://projectreactor.io/"} "Reactor"]
-                 " library, which implements the Reactive Streams Specifications, to asynchronously manage HTTP requests."]]
-   [:h3 "Requirements"]
-   [:p "Okay, let's assume we will build an HTTP service that does the following:"]
+   [:blockquote
+    [:p [:strong "Context: "] [:a {:href "https://spring.io/"} "Spring Framework 5"] " introduced the so-called "
+     [:strong "Reactive Stack"] ". The " [:code "reactive"] " keyword refers to the "
+     [:a {:href "https://www.reactivemanifesto.org/"} "Reactive Manifesto"]
+     ", which is a standard " [:a {:href "https://github.com/reactive-streams/reactive-streams-jvm/blob/v1.0.0/README.md"}
+                               "specification"] " for asynchronous stream processing with non-blocking back-pressure in JVM languages.
+     In short, Spring Webflux is a non-blocking web framework that uses " [:a {:href "https://projectreactor.io/"} "Reactor"]
+     " library, which implements the Reactive Streams Specifications, to asynchronously manage HTTP requests."]]
+   [:h2 "Requirements"]
+   [:p "Say we want to build an HTTP service that can do the following:"]
    [:ul
-    [:li [:code "POST /api/users/create"]]
-    [:li [:code "POST /api/users/fetch"]]
-    [:li [:code "POST /api/users/update-name"]]
-    [:li [:code "POST /api/users/delete"]]
-    [:li [:code "POST /api/users/fetch-all"] " (to demonstrate Mono vs. Flux)"]]
-   [:p "For any Spring Boot project, " [:a {:href "https://start.spring.io/"} "Spring Initializr" ] " is the go-to generators, like: " [:code "rails new blog"]]
+    [:li [:code "POST /api/users/create"] " (write a user record)"]
+    [:li [:code "POST /api/users/fetch"] " (read a user record)"]
+    [:li [:code "POST /api/users/all"] " (to demonstrate Mono vs. Flux)"]]
+   [:p "First, I would use "[:a {:href "https://start.spring.io/"} "Spring Initializr" ] " to bootstrap the project.
+   This helps setup our dependencies and gradle tasks necessary to get the server running "]
    [:ul
     [:li [:strong "Project: "] "Gradle Project"]
     [:li [:strong "Language: "] "Kotlin"]
-    [:li [:strong "Spring Boot: "] "2.1.3"]
+    [:li [:strong "Spring Boot: "] "2.2.0 (SNAPSHOT)"]
     [:li [:strong "Group: "] "com.dvliman"]
     [:li [:strong "Artifact: "] "demo"]
     [:li [:strong "Dependencies: "] "Reactive Web"]]
    [:p "Generate Project -  ⌘ + ⏎"]
-   [:p "Next, edit " [:code "build.gradle"], " add additional dependencies:"]
+   [:p "Next, let's add additional dependencies in " [:code "build.gradle"]]
    [:pre.code "
       dependencies {
           ...
           implementation 'com.github.davidmoten:rxjava2-jdbc:0.2.2'
           implementation 'org.postgresql:postgresql:42.2.5'
       }"]
-   [:p "At the time of writing, there are the 'official' reactive libraries like Spring Data Reactive Redis, Reactive MongoDB, etc
-   but not for JDBC drivers. JDBC is inherently a blocking API. "]
-   [:p "However, there are a couple third-party libraries
-   that uses non-blocking thread pool to execute blocking JDBC calls and exposes
-   reactive / reactor interfaces such as Mono and Flux. I picked David Moten's RxJava2 library
-   simply because it has good documentation and I can understand the code"]
-   [:strong [:h3 "Coding..."]]
+   [:blockquote [:p [:strong "Note: "] "At the time of writing, there is no \"official\" reactive JDBC drivers. JDBC is inherently a blocking API.
+    It will take while for JDBC async API to become a standard. However, there some third-party libraries that
+    we can use. These libraries typically use non-blocking thread pool to execute IO calls and exposes the
+    reactive / reactor interfaces such as Mono and Flux. I picked David Moten's RxJava2 library because it has good
+    documentation and I can understand the code"]]
+   [:h2 "Coding..."]
    [:p "Now, with all dependencies we need, we can start with writing configurations i.e
    your database connection url. Say we define a " [:code "config file."]]
    [:p "Then we add " [:code "Beans.kt"] ", which would contains all the beans in the your system"]
