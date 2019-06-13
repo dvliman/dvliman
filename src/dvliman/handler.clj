9dc3ea2d135d5ee8c88533a04f0150a33cec140b
David Liman <limanoit@gmail.com>
2019-06-13 15:04:20 -0700

    update: mrmrs remove its css

--- src/dvliman/handler.clj
+++ src/dvliman/handler.clj
@@ -3,13 +3,27 @@
             [compojure.route :as route]
             [ring.util.response :refer [response]]
             [hiccup.page :refer [html5 include-css]]
+            [hiccup.def :refer [defelem]]
+            [hiccup.element :refer [javascript-tag]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defn google-analytics []
+  (javascript-tag
+    (str
+      "window.dataLayer = window.dataLayer || [];"
+      "function gtag() { dataLayer.push(arguments); }"
+      "gtag('js', new Date());"
+      "gtag('set', {'user_id': 'USER_ID'});"
+      "gtag('config', 'UA-140213129-1');"
+      )))
 
 (defn base [title content]
   (html5 {:lang "en"}
     [:head
+     [:script {:src "https://www.googletagmanager.com/gtag/js?id=UA-140213129-1" :async true}]
+     (google-analytics)
      [:title title]
-     (include-css "//mrmrs.github.io/writing/css/tachyons.min.css")
+     (include-css "//unpkg.com/tachyons@4.10.0/css/tachyons.min.css")
      [:body content]]))
 
 (defn bullet-point [href title]
@@ -41,9 +55,8 @@
     [:h1.ttu.tracked.f3 "David Liman"]
     [:p.f3.lh-copy.measure
      "I am a (backend) Software Engineer living in Los Angeles.
-     I love solving complex problems with simple solutions"]
-    [:p.f3.lh-copy.measure
-     "I have experience working at different software businesses. See my " [:a.link.blue {:href "/work-experience.html"} "work experience."]]
+     I love solving complex problems with simple solutions. See my "
+     [:a.link.blue {:href "/work-experience.html"} "work experience."]]
     (links)
     (articles)]])
 
@@ -53,15 +66,18 @@
     [:h1.ttu.tracked.f3 "Work Experience"]
 
     [:h3.tracked.f4.bb.measure "Guardtime (2018 - Present)"]
-    [:p.f3.lh-copy.measure "I am designing supply chain solutions with Guardtime's proprietary digital timestamping service
-     called KSI (Keyless Signing Infrastructure). I am also looking into permissioned blockchain applications."]
+    [:p.f3.lh-copy.measure "I refactored an old Spring MVC application to Spring Boot with Webflux, Kotlin, and RxJava support ("
+    [:a {:href "https://medium.com/@dvliman/building-a-reactive-web-service-with-spring-webflux-kotlin-and-postgresql-71c4e0c2f870"}
+    "and blog about it"] ")
+    I participate in multiple contract-based projects. I am also
+    researching permissioned blockchain that fits the company's digital signatures / timestamping service."]
 
     [:h3.tracked.f4.bb.measure "Tigertext (2015 - 2018)"]
     [:p.f3.lh-copy.measure.
      "I was a Platform Engineer at TigerText in charge of a real-time messaging platform that was written in Erlang.
-     I worked on many product features such as chat rooms, chat bots, do-not-disturb auto-forwarding, CSV importer for nurses's schedules, file uploads, etc."]
+     I worked on many product features such as chat rooms, chat bots, do-not-disturb auto-forwarding, CSV importer for nurses's schedules, file uploads."]
 
-    [:p.f3.lh-copy.measure "As the business grows, we also helped evolved our chat server from only a secure messaging system to a communication
+    [:p.f3.lh-copy.measure "We also evolved our chat server from only a secure messaging system to a communication
      platform (moving off from XMPP to HTTP Server Sent Event with API support). We built a number of services such as
      SMTP email gateway, push notifications, document / image conversion system powered by a Redis-based worker queues"]
 
@@ -93,16 +109,15 @@
     [:p.f3.lh-copy.measure "The MapReduce job computes watch counts, video-view quartiles, device type, and
     a bunch of other dimensions that we use for reporting and billing purposes."]
 
-    [:p.f3.lh-copy.measure "Unfortunately, the company went under 6 months after I joined. I really enjoyed
-    the scales and technical challenges in ad-tech industry. "]
+    [:p.f3.lh-copy.measure "I really enjoyed the scales and technical challenges in ad-tech industry. "]
 
     [:h3.tracked.f4.bb.measure "IBM, Software Engineer (2011 - 2013)"]
     [:p.f3.lh-copy.measure "IBM was my first job out of college. I worked on a project called Lifecycle Query Engine. It indexes "
     [:a {:href "https://www.w3.org/RDF/"} "RDF triplets"] " and process " [:a {:href "https://en.wikipedia.org/wiki/SPARQL"} "SPARQL"] " queries"]
 
     [:p.f3.lh-copy.measure "I remember doing everything from building web UI with Dojo Toolkit (IBM's internal web framework)
-    to working on Java server deployed on Tomcat and IBM Websphere. I was tuning a bunch of knobs so that we can fetch
-    RDF resource as much as possible while serving simple-to-complex query templates"]]])
+    to working on Java server deployed on Tomcat and IBM Websphere. I was looking into some performance issues (aka tuning a bunch of knobs)
+    so that we can index RDF resources as much as possible while serving concurrent queries"]]])
 
 (defroutes app-routes
   (GET "/" [] (base "dvliman.com" (home)))
